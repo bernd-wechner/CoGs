18f691e1be35a52b0f972dc97f27ee4b7df83d4d
rwechner <rene.wechner@gmail.com>
2019-08-01 22:43:02 +1000

    Revert "Test of pull request"
    This reverts commit 44db76a999a568f122a4b262bafb6176e93aa876.

--- Leaderboards/tests.py
+++ Leaderboards/tests.py
@@ -1,5 +1,3 @@
 from django.test import TestCase
 
 # Create your tests here.
-
-# Here is a test line of a single local commit to cloned fork of Bernds CoGS, which ios the pull requested back to his master.
