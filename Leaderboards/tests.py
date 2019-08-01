44db76a999a568f122a4b262bafb6176e93aa876
rwechner <rene.wechner@gmail.com>
2019-08-01 22:42:37 +1000

    Test of pull request
    No functional change, just a contribution test

--- Leaderboards/tests.py
+++ Leaderboards/tests.py
@@ -1,3 +1,5 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# Here is a test line of a single local commit to cloned fork of Bernds CoGS, which ios the pull requested back to his master.
